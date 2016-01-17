f19d61dcf9540383bc3cf6beeb9d8a17c8d0c465
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-17 09:45:44 +0100

    Update server implementation to Express 4.*

--- src/tasklist/server.js
+++ src/tasklist/server.js
@@ -16,78 +16,95 @@
 
 // Module dependencies.
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
+var path = require('path');
 
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
-  azure = require('./../../lib/azure');
-} else {
-  azure = require('azure');
+try {
+    fs.statSync('./../../lib/azure.js');
+    azure = require('./../../lib/azure');
+} catch (error) {
+    azure = require('azure');
 }
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var multer = require('multer');
 var uuid = require('node-uuid');
 var Home = require('./home');
+var nconf = require('nconf');
 var ServiceClient = azure.ServiceClient;
-
-var app = module.exports = express.createServer();
-var client = azure.createTableService('UseDevelopmentStorage=true');
-
-// table creation
-client.createTableIfNotExists("tasks", function (res, created) {
-  if (created) {
-    var item = {
-      name: 'Add readonly todo list',
-      category: 'Site work',
-      date: '12/01/2011',
-      RowKey: uuid(),
-      PartitionKey: 'partition1',
-      completed: false
-    };
-
-    client.insertEntity("tasks", item, null, function () {
-      setupApplication();
+nconf.env()
+    .file({ file: './config.json', search: true })
+    .defaults({
+        "Azure": {
+            "Storage": {
+                "Table": "tasks"
+            }
+        },
+        "ViewEngine": "Jade"
     });
-  } else {
-    setupApplication();
-  }
+
+var app = module.exports = express();
+var storageAccount = nconf.get('Azure:Storage:AccountName');
+var storageAccessKey = nconf.get('Azure:Storage:AccessKey');
+
+// var client = azure.createTableService('UseDevelopmentStorage=true');
+var client = azure.createTableService(storageAccount, storageAccessKey);
+var table = nconf.get("Azure:Storage:Table");
+// table creation
+client.createTableIfNotExists(table, function (res, created) {
+    if (created) {
+        var item = {
+            name: 'Add readonly todo list',
+            category: 'Site work',
+            date: '12/01/2011',
+            RowKey: uuid(),
+            PartitionKey: 'partition1',
+            completed: false
+        };
+
+        client.insertEntity(table, item, null, function () {
+            setupApplication();
+        });
+    } else {
+        setupApplication();
+    }
 });
 
 function setupApplication() {
-  // Configuration
-  app.configure(function () {
+    // Configuration
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
+    // NOTE: Change configuration to use ejs instead of jade
+    app.set('view engine', nconf.get('ViewEngine'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(methodOverride());
 
-    // NOTE: Uncomment this line and comment next one to use ejs instead of jade
-    // app.set('view engine', 'ejs');
+    app.use(express.static(path.join(__dirname, '/public')));
 
-    app.set('view engine', 'jade');
+    if ('development' === app.get('env')) {
+        app.use(errorHandler());
+    }
+    if ('production' === app.get('env')) {
+        app.use(express.errorHandler());
+    }
 
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-  });
+    var home = new Home(client);
 
-  app.configure('development', function() {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  });
+    // Routes
 
-  app.configure('production', function() {
-    app.use(express.errorHandler());
-  });
+    app.get('/', home.showItems.bind(home));
+    app.get('/home', home.showItems.bind(home));
+    app.post('/home/newitem', home.newItem.bind(home));
+    app.post('/home/completed', home.markCompleted.bind(home));
 
-  var home = new Home(client);
+    app.listen(app.get('port'), function () {
+        console.log("Express server listening on port %d in %s mode",
+            app.get('port'),
+            app.get('env'));
+    });
 
-  // Routes
-
-  app.get('/', home.showItems.bind(home));
-  app.get('/home', home.showItems.bind(home));
-  app.post('/home/newitem', home.newItem.bind(home));
-  app.post('/home/completed', home.markCompleted.bind(home));
-
-  app.listen(process.env.PORT || 1337);
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 }
