dc6392ac6bc4b7d366e77c9971c11b716e84c1a6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-24 10:35:57 +0100

    Dev tools settings and changes

--- src/blobuploader/server.js
+++ src/blobuploader/server.js
@@ -1,18 +1,18 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 var fs = require('fs');
 var path = require('path');
@@ -143,7 +143,7 @@ app.post('/Delete/:id', function (req, res) {
 blobClient.createContainerIfNotExists(containerName, function (error) {
   if (error) {
     console.log(error);
-  } else { 
+  } else {
     setPermissions();
   }
 });
@@ -154,8 +154,8 @@ function setSAS(containerName, blobName) {
         AccessPolicy: {
             Expiry: azure.date.minutesFromNow(3)
         }
-    };   
-    
+    };
+
     var blobUrl = blobClient.getBlobUrl(containerName, blobName, sharedAccessPolicy);
     console.log("access the blob at ", blobUrl);
 }
