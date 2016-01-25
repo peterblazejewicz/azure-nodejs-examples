f2f930e3355d46f7a28d682f98b524a755db7ff6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-25 23:19:02 +0100

    Fix landing page markup

--- src/myevents/eventService.js
+++ src/myevents/eventService.js
@@ -14,104 +14,98 @@
 // limitations under the License.
 //
 
-var azure = require('azure')
-  , uuid = require('node-uuid');
+var azure = require('azure'),
+  uuid = require('node-uuid');
 module.exports = EventService;
 
-function EventService (tableClient, blobClient) {
-    this.tableClient = tableClient;
-    this.blobClient = blobClient;
+function EventService(tableClient, blobClient) {
+  this.tableClient = tableClient;
+  this.blobClient = blobClient;
 };
 
 EventService.prototype = {
 
-	showEvents: function(req, res) {
-		var self = this;
-		this.getEvents(function (resp, eventList) {
-            if (!eventList) {
-                eventList = [];
-            }
-            self.showResults(res, eventList);
+  showEvents: function (req, res) {
+    var self = this;
+    this.getEvents(function (error, result, response) {
+      self.showResults(res, result.entries);
+    });
+  },
+
+  showEvent: function (req, res) {
+    var self = this;
+    this.getEvent(req.params.id, function (resp, eventItem) {
+      if (!eventItem) {
+        res.render('error', {
+          title: "Error",
+          message: 'Event ' + req.params.id + ' not found.'
         });
-	},
-
-    showEvent: function(req, res) {
-        var self = this;
-        this.getEvent(req.params.id, function(resp, eventItem) {
-            if (!eventItem) {
-                res.render('error', {
-                   title: "Error",
-                   message: 'Event ' + req.params.id + ' not found.'
-                });
-            } else {
-                res.render('detail', {
-                    title: eventItem.name,
-                    eventItem: eventItem,
-                    imageUrl: self.blobClient.getBlobUrl('photos', eventItem.RowKey),
-                });
-            }
+      } else {
+        res.render('detail', {
+          title: eventItem.name,
+          eventItem: eventItem,
+          imageUrl: self.blobClient.getBlobUrl('photos', eventItem.RowKey),
         });
-    },
+      }
+    });
+  },
 
-  	getEvents: function(callback) {
-  		var query = azure.TableQuery
-              .select()
-              .from('events');
+  getEvents: function (callback) {
+    var query = new azure.TableQuery();
+    this.tableClient.queryEntities('events', query, null, callback);
+  },
 
-          this.tableClient.queryEntities(query, callback);
-  	},
+  getEvent: function (id, callback) {
+    this.tableClient.queryEntity('events', 'myEvents', id, callback);
+  },
 
-    getEvent: function(id, callback) {
-        this.tableClient.queryEntity('events', 'myEvents', id, callback);
-    },
+  showResults: function (res, eventList) {
+    res.render('index', {
+      title: 'My Events',
+      eventList: eventList
+    });
+  },
 
-    showResults: function (res, eventList) {
-        res.render('index', {
-            title: 'My Events',
-            eventList: eventList
-        });
-    },
+  newEvent: function (req, res) {
+    var self = this;
 
-    newEvent: function(req, res) {
-        var self = this;
+    var createEvent = function (resp, eventList) {
+      if (!eventList) {
+        eventList = [];
+      }
 
-        var createEvent = function(resp, eventList) {
-            if (!eventList) {
-                eventList = [];
-            }
+      var count = eventList.length;
 
-            var count = eventList.length;
+      var item = req.body.item;
+      item.RowKey = uuid();
+      item.PartitionKey = 'myEvents';
 
-            var item = req.body.item;
-            item.RowKey = uuid();
-            item.PartitionKey = 'myEvents';
+      self.tableClient.insertEntity('events', item, function (error) {
+        if (error) {
+          console.log(error);
+          throw error;
+        }
 
-            self.tableClient.insertEntity('events', item, function(error) {
-                if (error) {
-                   console.log(error);
-                   throw error;
-                }
+        console.log('event created, uploading photo.');
 
-                console.log('event created, uploading photo.');
-
-                var options = {
-                  contentType: req.files.item.file.type,
-                  metadata: { fileName: item.RowKey }
-                };
-
-                self.blobClient.createBlockBlobFromFile('photos', item.RowKey, req.files.item.file.path, options, function (error1, blockBlob, response) {
-                  if (error1) {
-                    throw error;
-                  } else {
-                    console.log(JSON.stringify(blockBlob));
-                    res.redirect('/');
-                  }
-                });
-
-
-            });
+        var options = {
+          contentType: req.files.item.file.type,
+          metadata: { fileName: item.RowKey }
         };
 
-        this.getEvents(createEvent);
-    },
+        self.blobClient.createBlockBlobFromFile('photos', item.RowKey, req.files.item.file.path, options, function (error1, blockBlob, response) {
+          if (error1) {
+            throw error;
+          } else {
+            console.log(JSON.stringify(blockBlob));
+            res.redirect('/');
+          }
+        });
+
+
+      });
+    };
+
+    this.getEvents(createEvent);
+  },
 };
