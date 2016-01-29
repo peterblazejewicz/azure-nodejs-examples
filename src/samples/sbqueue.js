fa2580549209cc6621a880e9b18308e9f5718c6d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-29 23:11:02 +0100

    First rewrite for Service Bus

--- src/samples/sbqueue.js
+++ src/samples/sbqueue.js
@@ -1,40 +1,36 @@
-﻿// 
+﻿//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
-
+var path = require('path');
+var util = require('util');
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
-  azure = require('./../../lib/azure');
-} else {
+try {
+  fs.statSync(path.join(__dirname, './../../lib/azure.js'));
+  azure = require(path.join(__dirname, './../../lib/azure'));
+} catch (error) {
   azure = require('azure');
 }
 
-var util = require('util');
-
 var queue = 'queuesample';
-
 var serviceBusClient = azure.createServiceBusService();
 
 function createQueue() {
   // Step 0: Create queue.
-  serviceBusClient.createQueue(queue, function (error) {
+  serviceBusClient.createQueueIfNotExists(queue, function (error, queue, response) {
     if (error) {
       console.log(error);
     } else {
@@ -46,14 +42,14 @@ function createQueue() {
 
 function sendMessages() {
   // Step 1: Send a few messages to later be consumed.
-  serviceBusClient.sendQueueMessage(queue, 'Send Message Works', function(error1) {
-    if (error1) {
-      console.log(error1);
+  serviceBusClient.sendQueueMessage(queue, 'Send Message Works', function (error, result, response) {
+    if (error) {
+      console.log(error);
     } else {
       console.log('Sent first Message');
-      serviceBusClient.sendQueueMessage(queue, 'Send Message Still Works', function(error2) {
-        if (error2) {
-          console.log(error2);
+      serviceBusClient.sendQueueMessage(queue, 'Send Message Still Works', function (error, result, reponse) {
+        if (error) {
+          console.log(error);
         } else {
           console.log('Sent Second Message');
           receiveMessages();
@@ -65,37 +61,37 @@ function sendMessages() {
 
 function receiveMessages() {
   // Step 2: Receive the messages.
-  serviceBusClient.receiveQueueMessage(queue, function (error1, message1) {
-    if (error1) {
-      console.log(error1);
+  serviceBusClient.receiveQueueMessage(queue, { isPeekLock: true }, function (error, message, response) {
+    if (error) {
+      console.log(error);
     } else {
-      console.log(message1.body);
-      serviceBusClient.receiveQueueMessage(queue, function (error2, message2) {
-        if (error2) {
-          console.log(error2);
+      console.log(message.body);
+      // Step 3: Remove/dequeue message
+      serviceBusClient.deleteMessage(message, function (error, response) {
+        if (error) {
+          console.log(error);
         } else {
-          console.log(message2.body);
+          // deleted
+          console.log(response);
         }
       });
     }
   });
 }
 
-var arguments = process.argv;
+var args = process.argv;
 
-if (arguments.length > 3) {
+if (args.length > 3) {
   console.log('Incorrect number of arguments');
-}
-else if (arguments.length == 3) {
+} else if (args.length == 3) {
   // Adding a third argument on the command line, whatever it is, will delete the container before running the sample.
-  serviceBusClient.deleteQueue(queue, function (error) {
+  serviceBusClient.deleteQueue(queue, function (error, deleted, response) {
     if (error) {
       console.log(error);
     } else {
       createQueue();
     }
   });
-}
-else {
+} else {
   createQueue();
 }
