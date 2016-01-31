b204e88d5f88bdb56d960d6826b10330dbe27b8e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-31 22:38:06 +0100

    SAS example [WIP]

--- src/samples/sassample.js
+++ src/samples/sassample.js
@@ -1,31 +1,31 @@
-﻿// 
+﻿//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 /**
 * In this sample, we demonstrate how to generate and use the blob level shared access signature and the container level
 * shared access signature.
-* 
-* In the blob level shared access signature sample, there are the following steps: 
-* 
+*
+* In the blob level shared access signature sample, there are the following steps:
+*
 * a. Create a container and a blob.
 *
 * b. Generate a shared access signature URL for the blob.
 *
 * c. Download that blob through that URL (should fail as the policy was never uploaded).
-* 
+*
 * In the container level shared access signature sample, there are the following steps:
 *
 * a. Create a container and a blob.
@@ -37,22 +37,18 @@
 */
 
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
-
+var path = require('path');
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
+try {
+  fs.statSync(path.join(__dirname, './../../lib/azure.js'));
   azure = require('./../../lib/azure');
-} else {
+} catch (error) {
   azure = require('azure');
 }
 
 var BlobConstants = azure.Constants.BlobConstants;
 var ServiceClient = azure.ServiceClient;
 var CloudBlobClient = azure.CloudBlobClient;
-
-var util = require('util');
 
 var container = 'sassample';
 var blob = 'sassample';
@@ -61,12 +57,11 @@ var blobService = azure.createBlobService();
 
 function createContainer() {
   // Step 0: Create the container.
-  blobService.createContainerIfNotExists(container, function (error) {
+  blobService.createContainerIfNotExists(container, function (error, result, response) {
     if (error) {
       console.log(error);
-    }
-    else {
-      console.log('Created the container ' + container);
+    } else {
+      console.log('Created the container %s', container);
       createBlob();
     }
   });
@@ -74,12 +69,11 @@ function createContainer() {
 
 function createBlob() {
   // Step 1: Create the blob
-  blobService.createBlockBlobFromText(container, blob, 'test blob', function (error) {
+  blobService.createBlockBlobFromText(container, blob, 'test blob', function (error, result, reponse) {
     if (error) {
       console.log(error);
-    }
-    else {
-      console.log('Created the blob ' + container);
+    } else {
+      console.log('Created the blob %s', container);
       downloadBlobUsingSharedAccessSignature();
     }
   });
@@ -87,7 +81,7 @@ function createBlob() {
 
 function downloadBlobUsingSharedAccessSignature() {
   var startDate = new Date();
-  var expiryDate = new Date(startDate);
+  var expiryDate = new Date(startDate.valueOf());
   expiryDate.setMinutes(startDate.getMinutes() + 5);
 
   var sharedAccessPolicy = {
@@ -107,11 +101,11 @@ function downloadBlobUsingSharedAccessSignature() {
   sharedAccessSignature.permissionSet = [signature];
 
   // Step 3: Download the blob by using the shared access signature URL. Since the read policy was never uploaded this should fail.
-  sharedBlobService.getBlobProperties(container, blob, function (error) {
+  sharedBlobService.getBlobProperties(container, blob, function (error, result) {
     if (error) {
       console.log('Failed to download the blob since the permission was invalid.');
     } else {
-      console.log('Downloaded the blob ' + blob + ' by using the shared access signature URL ' + signature.url());
+      console.log('Downloaded the blob %s by using the shared access signature URL %s', blob, signature.url());
     }
 
     createPolicies();
@@ -121,7 +115,7 @@ function downloadBlobUsingSharedAccessSignature() {
 function createPolicies() {
   // Step 4: Create a "ReadWrite" policy and a "Read" policy.
   var readWriteStartDate = new Date();
-  var readWriteExpiryDate = new Date(readWriteStartDate);
+  var readWriteExpiryDate = new Date(readWriteStartDate.valueOf());
   readWriteExpiryDate.setMinutes(readWriteStartDate.getMinutes() + 10);
 
   var readWriteSharedAccessPolicy = {
@@ -144,11 +138,11 @@ function createPolicies() {
   var options = {};
   options.signedIdentifiers = [readWriteSharedAccessPolicy, readSharedAccessPolicy];
 
-  blobService.setContainerAcl(container, BlobConstants.BlobContainerPublicAccessType.CONTAINER, options, function(error) {
+  blobService.setContainerAcl(container, BlobConstants.BlobContainerPublicAccessType.CONTAINER, options, function (error, result) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Uploaded the permissions for the container ' + container);
+      console.log('Uploaded the permissions for the container %s', container);
       usePermissions(readWriteSharedAccessPolicy, readSharedAccessPolicy);
     }
   });
@@ -164,11 +158,11 @@ function usePermissions(readWriteSharedAccessPolicy, readSharedAccessPolicy) {
 
   sharedAccessSignatureProvider.permissionSet = [sharedAccessSignature];
 
-  sharedBlobService.getBlobProperties(container, blob, function (error) {
+  sharedBlobService.getBlobProperties(container, blob, function (error, result) {
     if (error) {
       console.log('Failed to download the blob since the permission was invalid.');
     } else {
-      console.log('Downloaded the blob ' + blob + ' by using the shared access signature URL ' + sharedAccessSignature.url());
+      console.log('Downloaded the blob %s by using the shared access signature URL %s', blob, sharedAccessSignature.url());
     }
 
     useIncorrectPermission(readSharedAccessPolicy);
@@ -185,30 +179,28 @@ function useIncorrectPermission(readSharedAccessPolicy) {
 
   sharedAccessSignatureProvider.permissionSet = [sharedAccessSignature];
 
-  sharedBlobService.getBlobProperties(container, blob, function (error) {
+  sharedBlobService.getBlobProperties(container, blob, function (error, result) {
     if (error) {
       console.log('Failed to download the blob since the permission was invalid.');
     } else {
-      console.log('Downloaded the blob ' + blob + ' by using the shared access signature URL ' + sharedAccessSignature.url());
+      console.log('Downloaded the blob %s by using the shared access signature URL %s', blob, sharedAccessSignature.url());
     }
   });
 }
 
-var arguments = process.argv;
+var args = process.argv;
 
-if (arguments.length > 3) {
+if (args.length > 3) {
   console.log('Incorrect number of arguments');
-}
-else if (arguments.length == 3) {
+} else if (args.length === 3) {
   // Adding a third argument on the command line, whatever it is, will delete the container before running the sample.
-  blobService.deleteContainer(container, function (error) {
+  blobService.deleteContainerIfExists(container, function (error, result, response) {
     if (error) {
       console.log(error);
     } else {
       createContainer();
     }
   });
-}
-else {
+} else {
   createContainer();
 }
